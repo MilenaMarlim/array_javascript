999d5092ca13537f5c0768c52d9a6e7382d1beef
Milena Marlim <milenamarlim.dev@gmail.com>
2022-02-22 10:51:17 -0300

    Commit com os arquivos utilizados para construção do post do linkedin do assunto Array

---
+++ index.js
@@ -0,0 +1,23 @@
+//Array
+
+// let idades = [45, 23, 55]
+// let funcionarios = ['Ana', 'Robson', 'Marta']
+
+// console.log(idades)
+// console.log(funcionarios)
+
+let informacoesMisturadas = ['Carlos', 40, 'Rosa', 'Alberto', 60, 20]
+
+console.log(informacoesMisturadas)
+
+//Entendendo as posições dos elementos dentro de uma array
+
+let predio = ['Terreo', 'Primeiro Andar', 'Segundo Andar', 'Terceiro Andar', 'Quarto Andar']
+
+console.log(predio)
+
+//Chamando um elemento numa posição específica dentro de uma array
+
+console.log(predio[0])
+console.log(predio[2])
+
